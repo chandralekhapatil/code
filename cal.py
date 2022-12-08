2f28a454b12386af898f264017ab4cd31e8830e1
Chandralekha <chandralekhapatil0601@gmail.com>
2022-12-08 15:05:44 +0530

    secomnd commit

--- cal.py
+++ cal.py
@@ -14,29 +14,29 @@ def div(no1,no2):
     res=no1/no2
     print(f"Division:{res}")
 
-while True:
-    print(f"1.Addition ")
-    print(f"2.Substraction")
-    print(f"3.Division")
-    print(f"4.Division")
-    print(f"5.Exit")
-    ch=int(input("Enter choice:"))
-    if ch == 1:
-        no1=int(input("Enter no1:"))
-        no2=int(input("Enter no2:"))
-        add(no1,no2)
-    elif ch == 2:
-        no1=int(input("Enter no1:"))
-        no2=int(input("Enter no2:"))
-        sub(no1,no2)
-    elif ch == 3:
-        no1=int(input("Enter no1:"))
-        no2=int(input("Enter no2:"))
-        mul(no1,no2)
-    elif ch == 4:
-        no1=int(input("Enter no1:"))
-        no2=int(input("Enter no2:"))
-        div(no1,no2)
-    elif ch == 5:
-        break
+
+print(f"1.Addition ")
+print(f"2.Substraction")
+print(f"3.Division")
+print(f"4.Division")
+print(f"5.Exit")
+ch=int(input("Enter choice:"))
+if ch == 1:
+    no1=int(input("Enter no1:"))
+    no2=int(input("Enter no2:"))
+    add(no1,no2)
+elif ch == 2:
+    no1=int(input("Enter no1:"))
+    no2=int(input("Enter no2:"))
+    sub(no1,no2)
+elif ch == 3:
+    no1=int(input("Enter no1:"))
+    no2=int(input("Enter no2:"))
+    mul(no1,no2)
+elif ch == 4:
+    no1=int(input("Enter no1:"))
+    no2=int(input("Enter no2:"))
+    div(no1,no2)
+elif ch == 5:
+    exit
     
